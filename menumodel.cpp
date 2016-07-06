9c62e60e682c29dedc7e38f80d279a51a285665c
Sedfer <balovilya@mail.ru>
2016-07-07 00:49:47 +0300

    Added error messages to non-implemented slots

--- menumodel.cpp
+++ menumodel.cpp
@@ -1,4 +1,7 @@
 #include "inc/menumodel.h"
+#include <iostream>
+
+using namespace std;
 
 MenuModel::MenuModel(QObject *parent)
     : QObject(parent)
@@ -9,30 +12,30 @@ MenuModel::MenuModel(QObject *parent)
 
 void MenuModel::clickedPlay()
 {
-
+    cerr << "MenuModel::clickedPlay()" << endl;
 }
 
 void MenuModel::clickedPlayOnline()
 {
-
+    cerr << "MenuModel::clickedPlayOnline()" << endl;
 }
 
 void MenuModel::clickedPlayCPU()
 {
-
+    cerr << "MenuModel::clickedPlayCPU()" << endl;
 }
 
 void MenuModel::clickedExit()
 {
-
+    cerr << "MenuModel::clickedExit()" << endl;
 }
 
 void MenuModel::clickedSound()
 {
-
+    cerr << "MenuModel::clickedSound()" << endl;
 }
 
 void MenuModel::clickedMusic()
 {
-
+    cerr << "MenuModel::clickedMusic()" << endl;
 }
