f5d7fa8d6644ccd8b681e228edb472faede41b34
Sedfer <balovilya@mail.ru>
2016-07-16 00:38:34 +0300

    Added effect to Hero

--- battlemodel.cpp
+++ battlemodel.cpp
@@ -48,6 +48,17 @@ void BattleModel::beginTurn()
     heroQueue.rotate();
     emit setQueue(&heroQueue);
     emit setActiveHero(heroQueue.first());
+
+    if( heroQueue.first()->onTurnEffects() ) // if skip turn
+    {
+        beginTurn();
+        return;
+    }
+
+    if( heroQueue.first()->updateDead() )
+    {
+        emit showDead(heroQueue.first());
+    }
 }
 
 void BattleModel::updateDead()
